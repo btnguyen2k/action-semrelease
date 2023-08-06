49aa60ef3835b778ef247751652a46ffc56cbca8
Thanh Nguyen <thanhn@microsoft.com>
2023-08-07 01:28:31 +1000

    test action

--- src/index.js
+++ src/index.js
@@ -1,21 +1,34 @@
 const core = require('@actions/core')
+const github = require('@actions/github')
 const parseReleaseNotes = require('./release-notes')
 
 // most @actions toolkit packages have async methods
 async function run() {
   try {
-    const isDryRun = core.getInput('dry-run') || 'false'
+    const inputDryRun = core.getInput('dry-run') || 'false'
+    const isDryRun = inputDryRun.toLowerCase() === 'true'
+
     const releaseNotes = parseReleaseNotes()
     if (releaseNotes === undefined) {
       throw new Error('No release version/notes found')
     }
-    if (isDryRun.toLowerCase() === 'true') {
-      core.info(`Release version: ${releaseNotes.release_version}`)
-      core.info(`Release notes:\n${releaseNotes.release_notes}`)
-      return
-    }
+    core.info(`Release version: ${releaseNotes.release_version}`)
+    core.info(`Release notes:\n${releaseNotes.release_notes}`)
     core.setOutput('releaseVersion', releaseNotes.release_version)
     core.setOutput('releaseNotes', releaseNotes.release_notes)
+
+    const githubToken = core.getInput('github-token')
+    const octokit = github.getOctokit(githubToken)
+    const releaseTag = await octokit.rest.tags.get({
+      owner: github.context.repo.owner,
+      repo: github.context.repo.repo,
+      ref: `releases/tag/v${releaseNotes.release_version}`
+    })
+    core.info(`Major tag: ${releaseTag}`)
+
+    if (!isDryRun) {
+      return
+    }
   } catch (error) {
     core.setFailed(error.message)
   }
